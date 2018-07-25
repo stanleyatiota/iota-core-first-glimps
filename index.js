6cd3e54826ea9d0aef0484a371a8fdfb8cd21e2c
stanleyatiota <40600169+stanleyatiota@users.noreply.github.com>
2018-07-25 13:57:41 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 
-
-import { composeAPI } from '@iota/core'
+import { composeAPI, generateAddress } from '@iota/core'
 
 const iota = composeAPI({
-    provider: 'https://nodes.testnet.iota.org:443'
+  //  provider: 'https://nodes.testnet.iota.org:443'
+    provider: 'https://field.carriota.com:443'
 })
 
-console.log("Hello")
 iota.getNodeInfo()
-    .then(info => console.log(info))
-    .catch(err => {console.log("error="+err)})
+    .then(info => {
+        console.log(info);
+        if (Math.abs(info['latestMilestoneIndex'] - info['latestSolidSubtangleMilestoneIndex']) > 3) {
+            console.log('\r\nNode is probably not synced!');
+        } else {
+            console.log('\r\nNode is probably synced!');
+            GetMyBalance();
+        }
+
+    })
+    .catch(err => { console.log("get NodeInfo error=" + err) });
+
+
+function GetMyBalance() {
+    let MySeed = "WKQDUZTGFKSSLACUCHHLZRKZBHSDSCEBHKUPDLKFBQALEBKDMFRPUQGZRXAADPG9TSRTZGGBZOFRJCFMM";
+
+    const myaddress1 = generateAddress(MySeed, 1);
+    console.log("My address1: " + myaddress1)
+
+    iota.getBalances([myaddress1], 100).then(({ balances }) => {
+        console.log("Balance: " + balances)
+    }).catch(err => { console.log("getBances err: " + err) })
+}
